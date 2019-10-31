f9fd032001e6fc5cd6a615370a26f039218c027d
ChibiKev <k3v.ch3n@gmail.com>
2019-10-31 19:53:13 -0400

    FCFS Done. Added A Print

--- Disk-Scheduling.c
+++ Disk-Scheduling.c
@@ -4,8 +4,8 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define DISKNUM 10000
-#define CYLINDERNUM 1000
+#define DISKNUM 5
+#define CYLINDERNUM 3
 
 int cylinders[CYLINDERNUM];
 
@@ -25,7 +25,7 @@ void cylindersRequest(){ // Obtain Random Cylinder Requests
 				}
 			}
 			if(found == false){ // Not Fount After Looping Through Array
-				//printf("%d- random: %d \n", i, random);
+				printf("%d- random: %d \n", i, random);
 				cylinders[i] = random; // Requesting From a Range From 0-9999
 				break; // Unique Is True So i++. Break.
 			}
@@ -33,40 +33,52 @@ void cylindersRequest(){ // Obtain Random Cylinder Requests
 	}
 }
 
-void FCFS(int cylinders[],int head){
-
+int FCFS(int cylinders[],int head){
+	int sum = 0;
+	for(int i = 0; i < CYLINDERNUM; i++){
+		sum += abs(head - cylinders[i]);
+		head = cylinders[i];
+	}
+	return sum;
 }
 
-void SSTF(int cylinders[],int head){
-
+int SSTF(int cylinders[],int head){
+	return 0;
 }
 
-void SCAN(int cylinders[],int head){
-
+int SCAN(int cylinders[],int head){
+	return 0;
 }
 
-void CSCAN(int cylinders[],int head){
-
+int CSCAN(int cylinders[],int head){
+	return 0;
 }
 
-void LOOK(int cylinders[],int head){
-
+int LOOK(int cylinders[],int head){
+	return 0;
 }
 
 int main(){
 	cylindersRequest(); // Obtain a Random Series of 1000 Cylinders Requests
 	int input; // To Store Disk Head
-	printf("Insert Initial Position of the Disk Head Between 0 and 9,999: "); // Print
+	printf("Insert Initial Position of the Disk Head Between 0 and %d: ", CYLINDERNUM - 1); // Print
 	scanf("%d", &input); // Obtain Initial Position of the Disk Head
 	while(input < 0 || input > CYLINDERNUM - 1){ // Makes Sure Input Is Appropriate
-		printf("Incorrect Input. Input Has to be Between 0 and 9,999. You Inputted %d.\n", input); // Print
-		printf("Insert Initial Position of the Disk Head Between 0 and 9,999: "); // Print
+		printf("Incorrect Input. Input Has to be Between 0 and %d. You Inputted %d.\n", CYLINDERNUM - 1, input); // Print
+		printf("Insert Initial Position of the Disk Head Between 0 and %d: ", CYLINDERNUM -1); // Print
 		scanf("%d", &input); // Obtain Initial Position of the Disk Head
 	}
-	FCFS(cylinders,input);
-	SSTF(cylinders,input);
-	SCAN(cylinders,input);
-	CSCAN(cylinders,input);
-	LOOK(cylinders,input);
+	int FCFS_SUM = FCFS(cylinders,input);
+	int SSTF_SUM = SSTF(cylinders,input);
+	int SCAN_SUM = SCAN(cylinders,input);
+	int CSCAN_SUM = CSCAN(cylinders,input);
+	int LOOK_SUM = LOOK(cylinders,input);
+	printf("Algorithm\t|Total Head Movement\n");
+	printf("________________|___________________\n");
+	printf("FCFS\t\t|%d\n", FCFS_SUM);
+	printf("SSTF\t\t|%d\n", SSTF_SUM);
+	printf("SCAN\t\t|%d\n", SCAN_SUM);
+	printf("CSCAN\t\t|%d\n", CSCAN_SUM);
+	printf("LOOK\t\t|%d\n", LOOK_SUM);
 	return 0;
 }
